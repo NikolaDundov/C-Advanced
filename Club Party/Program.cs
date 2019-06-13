add42905b3ebd6151a6c21d8aac22d3749b036db
Никола Дундов <40323955+NikolaDundov@users.noreply.github.com>
2019-06-13 23:22:54 +0300

    Add files via upload

---
+++ Club Party/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Club_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            var line = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+
+
+            var hallsNames = new Queue<string>();
+            var enteredGuests = new List<int>();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                string currentInput = line[i];
+
+                if (currentInput.Length == 1 && char.IsLetter(char.Parse(currentInput)))
+                {
+                    hallsNames.Enqueue(currentInput);
+                }
+
+                else
+                {
+                    int currentGroup = int.Parse(currentInput);
+
+                    if (hallsNames.Count > 0 && (enteredGuests.Sum() + currentGroup) <= maxCapacity)
+                    {
+                        enteredGuests.Add(int.Parse(currentInput));
+                    }
+
+                    else if (hallsNames.Count > 0)
+                    {
+                        Console.WriteLine($"{hallsNames.Dequeue()} -> {string.Join(", ", enteredGuests)}");
+                        enteredGuests.Clear();
+
+                        if (hallsNames.Count > 0)
+                        {
+                            enteredGuests.Add(currentGroup);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
