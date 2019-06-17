d6b1e06a20af11f08661e0a5f098d63f01e4d013
Никола Дундов <40323955+NikolaDundov@users.noreply.github.com>
2019-06-17 23:47:15 +0300

    Add files via upload
    3 task from demo exam 16.06.2019

---
+++ 03. Healthy Heaven_Skeleton/HealthyHeaven/Restaurant.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HealthyHeaven
+{
+    public class Restaurant
+    {
+        private List<Salad> data;
+        public string Name { get; private set; }
+
+        public Restaurant(string name)
+        {
+            this.Name = name;
+            this.data = new List<Salad>();
+        }
+
+        public void Add(Salad salad)
+        {
+            data.Add(salad);
+        }
+
+        public bool Buy(string name)
+        {
+            if (this.data.FirstOrDefault(x => x.Name == name) == null)
+            {
+                return false;
+            }
+
+            data.Remove(this.data.FirstOrDefault(x => x.Name == name));
+            return true;
+        }
+
+        public string GetHealthiestSalad()
+        {
+            Salad healties = null;
+            healties = data.OrderBy(x => x.GetTotalCalories()).FirstOrDefault();
+            return healties.Name;
+        }
+
+
+        public string GenerateMenu()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Name} have {this.data.Count} salads:");
+
+            foreach (var item in data)
+            {
+                sb.AppendLine($"{item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03. Healthy Heaven_Skeleton/HealthyHeaven/Salad.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HealthyHeaven
+{
+    public class Salad
+    {
+        private List<Vegetable> products;
+        public string Name { get; private set; }
+        
+
+        public Salad(string name)
+        {
+            this.Name = name;
+            this.products = new List<Vegetable>();
+        }
+
+        public int GetTotalCalories()
+        {
+            int totalCal = 0;
+            return totalCal = products.Sum(x => x.Calories);
+
+        }
+
+        public int GetProductCount()
+        {
+            return products.Count;
+        }
+
+
+        public void Add(Vegetable product)
+        {
+            products.Add(product);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"* Salad {Name} is {GetTotalCalories()} calories and have {GetProductCount()} products:");
+
+            foreach (var product in products)
+            {
+                sb.AppendLine(product.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
